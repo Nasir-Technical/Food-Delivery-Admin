b99dd54213d4c51f05b2a0faa941cefac0e5b2f8
Nasir-Technical <nasir.18.2006@gmail.com>
2024-08-23 11:57:46 +0500

    changes

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'http://localhost:4000'; // Your backend URL
+// export const BACKEND_URL = 'http://localhost:4000'; // Your backend URL
+export const BACKEND_URL = 'https://mr-food-del.vercel.app/'; // Your backend URL
