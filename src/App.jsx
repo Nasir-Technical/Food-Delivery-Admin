d773b1c35c348334c2e5d4e5084c21e24d2a79d6
Nasir-Technical <nasir.18.2006@gmail.com>
2025-04-22 01:37:47 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import AdminAccess from './components/AdminAccess/AdminAccess';
 
 const App = () => {
   const [token, setToken] = useState(null);
-  const url = "https://food-del-indol.vercel.app";
+  const url = "https://food-del-ashen-ten.vercel.app";
   const navigate = useNavigate();
 
   useEffect(() => {
